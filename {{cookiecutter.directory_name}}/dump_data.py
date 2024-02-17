9b82604957bab7988d8c06ece8e797a14eadcb96
Peter Andorfer <peter.andorfer@oeaw.ac.at>
2024-02-17 15:43:41 +0100

    Update dump_data.py

--- {{cookiecutter.directory_name}}/dump_data.py
+++ {{cookiecutter.directory_name}}/dump_data.py
@@ -3,5 +3,5 @@ from config import br_client, BASEROW_DB_ID, JSON_FOLDER
 
 os.makedirs(JSON_FOLDER, exist_ok=True)
 
-files = br_client.dump_tables_as_json(BASEROW_DB_ID, folder_name="json_dumps", indent=2)
+files = br_client.dump_tables_as_json(BASEROW_DB_ID, folder_name=JSON_FOLDER, indent=2)
 print(files)
